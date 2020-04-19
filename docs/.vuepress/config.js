229d8d0e1e3b01ddae4d90de48fda562139f463a
lemon <1192001561@qq.com>
2020-04-19 16:04:47 +0800

    feat: pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 moment.locale("zh-cn");
 
 module.exports = {
-	base: "/vuepress/",
+	// base: "/vuepress/",
 	title: "飞跃高山与大海的鱼",
 	description: "飞跃高山与大海的鱼的笔记,第二次",
 	head: [
@@ -15,7 +15,15 @@ module.exports = {
 		['meta', {
 			name: 'keywords',
 			content: 'vuepress介绍, vuepress说明,飞跃高山与大洋的鱼'
-		}]
+		}],
+		 ['link', { rel: 'manifest', href: '/manifest.json' }],
+				['meta', { name: 'theme-color', content: '#3eaf7c' }],
+				['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+				['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+				['link', { rel: 'apple-touch-icon', href: '/icons/icon-152x152.png' }],
+				['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+				['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
+				['meta', { name: 'msapplication-TileColor', content: '#000000' }]
 	],
 	plugins: [
 	    [
@@ -28,7 +36,16 @@ module.exports = {
 	          return moment(timestamp).format("LLLL")
 	        }
 	      }
-	    ]
+	    ],
+		[
+			'@vuepress/pwa', {
+			      serviceWorker: true,
+			      updatePopup: {
+			        message: "大掌柜,发现新的功能可用",
+			        buttonText: "刷新"
+			      }
+			   }
+		]
 	  ],
 
 	themeConfig: {
