b7d09747e32c92dd154e8ce0131ac07a9d044fb8
lemon <1192001561@qq.com>
2020-04-19 11:04:54 +0800

    feat: 发不到github.io

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,9 @@
 // .vuepress/config.js
+const moment = require('moment');
+moment.locale("zh-cn");
+
 module.exports = {
+	base: "/vuepress/",
 	title: "飞跃高山与大海的鱼",
 	description: "飞跃高山与大海的鱼的笔记,第二次",
 	head: [
@@ -13,6 +17,19 @@ module.exports = {
 			content: 'vuepress介绍, vuepress说明,飞跃高山与大洋的鱼'
 		}]
 	],
+	plugins: [
+	    [
+	      '@vuepress/last-updated',
+	      {
+	        transformer: (timestamp, lang) => {
+	          // 不要忘了安装 moment
+	          
+	          // moment.locale(lang)
+	          return moment(timestamp).format("LLLL")
+	        }
+	      }
+	    ]
+	  ],
 
 	themeConfig: {
 		lastUpdated: '更新时间', // string | boolean
